8209b1f4f136e0c5dabd6ffb7d6f68cbcb090acb
cldrojas <cld.rojasz@gmail.com>
2023-03-05 23:51:01 -0300

    WIP: update tech stack

---
+++ efe/src/components/Logo.svelte
@@ -0,0 +1,14 @@
+<div class="logo"><a href="#Hero">Verónica</a></div>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Pattaya&display=swap");
+  .logo {
+    font-family: "Pattaya", sans-serif;
+    font-size: 40px;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--morado);
+  }
+</style>

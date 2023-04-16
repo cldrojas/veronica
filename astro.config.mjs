8ae9cad62eb0560f5c2cb614d4ce6105fe446895
cldrojas <cld.rojasz@gmail.com>
2023-04-15 20:20:35 -0400

    another try

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import svelte from '@astrojs/svelte'
 
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  site: 'https://cldrojas.github.io/veronica',
-  base: '/'
+  site: 'https://cldrojas.github.io/veronica'
 })

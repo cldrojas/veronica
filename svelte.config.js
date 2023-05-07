250b3296e918b8b0027830e8955eb3920bc95958
cldrojas <cld.rojasz@gmail.com>
2023-05-07 19:06:19 -0400

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
 import { vitePreprocess } from '@astrojs/svelte'
 
 export default {
-  preprocess: vitePreprocess(),
-  compilerWarnings: {
-    'a11y-click-events-have-key-events': 'ignore'
-  }
+  preprocess: vitePreprocess()
 }

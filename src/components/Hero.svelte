571c86c494be62bc79acd9c695cfddece8088ac4
cldrojas <cld.rojasz@gmail.com>
2023-05-07 23:08:32 -0400

    Update: pages have new order - Welcome page with photo and quote - Services page listing services by category - Aboutpage to give more info about Veronica - Contact page with form and links
    Extra: removed unused svelte components and favicon

--- src/components/Hero.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import bgHero from '../../assets/bg-hero.jpg'
-</script>
-
-<section id="Hero" class="container flex items-center justify-center p-3">
-	<img src={bgHero} alt="Verónica Crespo Perez" 
-	class="max-w-full h-auto transition-shadow border rounded-lg">
-	
-</section>

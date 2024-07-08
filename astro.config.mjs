67e3b084efd6f3dedd9bc71d0f8bf4be03f7ed37
cldrojas <cld.rojasz@gmail.com>
2024-07-07 23:01:00 -0400

    fresh start using astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  site: 'https://cldrojas.github.io',
-  base: '/veronica'
+  integrations: [tailwind(), svelte()]
 })

a91a5a00c6a3f19f62e6a4c246de6e0da598617b
cldrojas <cld.rojasz@gmail.com>
2024-07-21 10:56:35 -0400

    fix config files and remove unused ones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
 
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind()]
 })
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export function classList (classes) {
-  return Object.entries(classes)
-    .filter(([className, active]) => active)
-    .map(([className]) => className)
-    .join(' ')
-}
